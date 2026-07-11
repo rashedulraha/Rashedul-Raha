df7fe2d295733bbb154000b7cdf97a61d9554245
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-07-11 14:30:39 +0600

    refactor(layout): update Navbar import to point to new directory

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar/Navbar";
 import Chatbot from "@/components/Chatbot";
 
 export default function MainLayout({
