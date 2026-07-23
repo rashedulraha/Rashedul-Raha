6efff0b9c2376da84f26b31575f0b9252d0790ff
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-07-23 10:35:18 +0600

    refactor: remove i18n configuration and language switchers

--- src/proxy.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './routing';
-
-export const proxy = createMiddleware(routing);
-
-export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(de|en|bn|ja|es|fr|it)/:path*']
-};
